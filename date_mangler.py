029f82b1275e152ab042a66b1f91952e76368110
Cubed <47624167+cubed-guy@users.noreply.github.com>
2024-02-18 19:40:04 +0530

    Added monthdelta. And, it's good!

--- date_mangler.py
+++ date_mangler.py
@@ -1,5 +1,6 @@
 from enum import Enum, auto
 from datetime import datetime as dt, timedelta as td
+from dateutil.relativedelta import relativedelta as rd
 
 class Operions(Enum):
 	add = auto()
@@ -21,9 +22,85 @@ class Operions(Enum):
 		except TypeError:
 			return op2
 
-class CommandProcessor:
+class monthdelta:
+	'''
+	An intermediate date representation that collapses into a real datetime object
+	when it is added with a timedelta object. More info in __add__.
+	'''
+
 	UNIX_START_YEAR = 1970
 
+	def __init__(self, months=0, *, years=0):
+		self.months = months + years * 12
+
+	def __repr__(self):
+		return f'{self.__class__.__name__}(months={self.months})'
+
+	def __str__(self):
+		y, m = divmod(self.months, 12)
+		return f'{y} years, {m} months'  # no plural stuff; it's fine
+
+	def __add__(self, other, default_year = 1):
+		'''
+		Adds two time objects together.
+
+		Adding a datetime.timedelta object will result in a datetime.dateime object
+		created from the year and month corresponding to this monthdelta object
+		and the timedelta object will be added to it.
+
+		monthdelta(years=2020, months=6) + td(days=13) == datetime(year=2020, month=6, day=13)
+
+		The actual logic of calculating this as follows:
+		monthdelta(years=y, months=m) + td(days=d) = datetime(year=y, month=m, day=1) + td(days=d) - td(days=1)
+
+		This leads to some edge cases that may be confusing:
+		monthdelta(years=2020, months=6) + td(days=0) == datetime(year=2020, month=5, day=31)
+		monthdelta(years=2020, months=6) + td(days=-1) == datetime(year=2020, month=5, day=30)
+		'''
+
+		if isinstance(other, self.__class__):
+			return self.__class__(self.months + other.months)
+
+		if isinstance(other, dt):
+			out = other + rd(months=self.months)
+			# print(out)
+			return out
+
+		if isinstance(other, td):
+			y, m = divmod(self.months, 12)
+			if m == 0: m = 12; y -= 1
+			if y <= 0: y = default_year  # cuz year 0 is not supported.
+			# converts to dt
+			return dt(year=y, month=m, day=1)+other-td(1)
+
+		raise TypeError(f'Cannot add {self.__class__.__name__!r} with {other.__class__.__name__!r}')
+
+	def __radd__(self, other):
+		return self + other
+
+	def __rsub__(self, other):
+		if isinstance(other, self.__class__):
+			return self.__class__(other.months - self.months)
+
+		if isinstance(other, dt):
+			months = other.month + other.year*12 - self.months
+
+			y, m = divmod(months, 12)
+
+			return other.replace(year=y, month=m)
+
+		raise TypeError(f'Cannot subtract {self.__class__.__name__!r} from {other.__class__.__name__!r}')
+
+	def __mul__(self, other):
+		return self.__class__(self.months * other)
+
+	def __truediv__(self, other):
+		if not isinstance(other, self.__class__):
+			raise TypeError(f'Cannot divide {self.__class__.__name__} by {other.__class__.__name__}')
+
+		return self.months / other.months
+
+class CommandProcessor:
 	def __init__(self):
 		self.val = 0
 		self.operation = Operions.add
@@ -54,12 +131,28 @@ class CommandProcessor:
 			elif c == '_': op2 = -self.multiplier
 
 			elif c == 'm':
-				op2 = dt(
-					year=self.UNIX_START_YEAR, month=self.multiplier, day=1
-				) - td(1)
+				op2 = monthdelta(months=self.multiplier)
+				self.multiplier = 1
 
 			elif c == 'y':
-				op2 = dt(year=self.multiplier, month=1, day=1) - td(1)
+				op2 = monthdelta(years=self.multiplier)
+				self.multiplier = 1
+
+			elif c == 'Y':  # force the year field
+				if isinstance(self.val, dt):
+					self.val = self.val.replace(year=self.multiplier)
+					self.multiplier = 1
+					continue
+
+				if isinstance(self.val, monthdelta):
+					m = self.val.months%12
+					if m == 0: m = 12
+					self.val = monthdelta(years=self.multiplier, months=m)
+					self.multiplier = 1
+					continue
+
+				op2 = monthdelta(years=self.multiplier)
+				self.multiplier = 1
 
 			elif c == 'u':
 				op2 = dt.utcfromtimestamp(self.multiplier)
